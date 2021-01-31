fd829033a96e9adbd580d4d66783440c4a2427b1
Siddharth Maity <maitys@outlook.com>
2021-01-31 00:02:36 -0500

    bioinformatics project

--- project_1/1_stock_price.py
+++ project_1/1_stock_price.py
@@ -7,11 +7,11 @@ Shown are the stock closing price and volume of Gamestop!
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
+# define the ticker symbol
 tickerSymbol = 'GME'
-#get data on this ticker
+# get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
+# get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-1-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
